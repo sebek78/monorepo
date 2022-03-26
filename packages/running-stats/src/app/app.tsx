1c361bf8b4bcb531f4ea503a74dbe35352fa47c7
sebek78 <sebasporek@gmail.com>
2022-03-26 11:37:02 +0100

    Add React

---
+++ packages/running-stats/src/app/app.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+// import { Route, Link } from 'react-router-dom';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return <StyledApp>Running Stats</StyledApp>;
+}
+
+export default App;
